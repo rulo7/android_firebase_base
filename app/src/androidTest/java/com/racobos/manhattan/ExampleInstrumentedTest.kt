a3d3f0e6a494b0b48ac681459a94b94a662bdf87
rulo7 <rulo2305@gmail.com>
2018-11-17 14:13:28 +0100

    - Renamed teammus by manhattan

--- app/src/androidTest/java/com/racobos/manhattan/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/racobos/manhattan/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.racobos.teammus", appContext.packageName)
+        assertEquals("com.racobos.manhattan", appContext.packageName)
     }
 }
